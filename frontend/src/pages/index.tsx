89ac4dfd360c6c03f1357dc0c8b9df32badd75fd
jim-low <robil12380@gmail.com>
2025-06-01 14:11:41 +0800

    feat: some changes

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,22 @@
+import { PageContainer } from '@ant-design/pro-layout';
+import { useEffect } from 'react';
+import { history } from 'umi';
 import styles from './index.less';
 
 export default function IndexPage() {
+  useEffect(() => {
+    history.push({
+      pathname: '/haha-go-brrr',
+      query: {
+        foo: 'bar',
+        baz: '69',
+      }
+    });
+  }, []);
+
   return (
-    <div>
+    <PageContainer loading={false}>
       <h1 className={styles.title}>Page index</h1>
-    </div>
+    </PageContainer>
   );
 }
